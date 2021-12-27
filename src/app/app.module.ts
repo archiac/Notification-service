a32e3af927816d66a8345e045898cd3574885da5
archiac <marchenko.mac@gmail.com>
2021-12-27 13:43:15 +0300

    add simple input

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,18 +7,29 @@ import { HeaderComponent } from './core/header/header.component';
 import { HomeComponent } from './modules/home/home.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { FooterComponent } from './core/footer/footer/footer.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { InputFormComponent } from './shared/components/input-form/input-form.component';
+import {MatInputModule} from '@angular/material/input';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatSelectModule} from '@angular/material/select';
+
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     HomeComponent,
-    FooterComponent
+    FooterComponent,
+    InputFormComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    NgbModule
+    NgbModule,
+    BrowserAnimationsModule,
+    MatInputModule,
+    MatFormFieldModule,
+    MatSelectModule
   ],
   providers: [],
   bootstrap: [AppComponent]
